f5d4f2f4e6cc0f37178df23e9890e3b4f18882ae
Greg <genius242424@gmail.com>
2020-05-27 16:43:05 +0200

    temp_products

---
+++ js/carousel_products.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $('.carousel_container').slick({
+      infinite: true,
+      slidesToShow: 5,
+      autoplay: true,
+      responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+      infinite: true,
+      slidesToShow: 4,
+          autoplay: true,
+      }
+    },
+    {
+      breakpoint: 1100,
+      settings: {
+      infinite: true,
+      slidesToShow: 3,
+          autoplay: true,
+      }
+    },
+    
+    {
+      breakpoint: 900,
+      settings: {
+      slidesToShow: 3,
+          centerMode:true,
+          arrows: false,
+          autoplay: true,
+          infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+      slidesToShow: 2,
+          centerMode: true,
+          arrows: false,
+          autoplay: true,
+          infinite: true,
+      }
+    }
+    ]
+  });
+});
